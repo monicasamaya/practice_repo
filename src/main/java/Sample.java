a3e220536ead1341ca6965de3a82c3634236316f
monicasamaya <monicasamaya5@email.com>
2026-03-31 11:09:26 +0530

    change in branch 1

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -10,6 +10,10 @@ public class Sample {
         System.out.println("Difference: " + (b - a));
         System.out.println("Product: " + (a * b));
         System.out.println("Division: " + (b / a));
+
         System.out.println("Happy learning");
+
+        System.out.println("checking rebase");
+
     }
 }
