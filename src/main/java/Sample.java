480170211abe7ec395b4b60657b3a70d554a2540
Monica Samaya <155799038+monicasamaya@users.noreply.github.com>
2026-03-31 12:47:00 +0530

    Clean up output in Sample.java
    Removed unnecessary print statements and comments.

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,19 +1,11 @@
 public class Sample {
     
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Lets do a Quick Math");
-        // Simple arithmetic operations
         int a = 10;
         int b = 300;//changed
         System.out.println("Sum: " + (a + b));
         System.out.println("Difference: " + (b - a));
         System.out.println("Product: " + (a * b));
         System.out.println("Division: " + (b / a));
-
-        System.out.println("Happy learning");
-
-        System.out.println("checking rebase");
-        System.out.println("Thank you!");
     }
 }
